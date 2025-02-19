656d3b1b049d3e4c85985db61436f30b739127c4
Cozy-Habit <sophia.precker77@gmail.com>
2025-02-19 12:23:33 +0100

    update workflow with error

--- src/app/__tests__/page.test.jsx
+++ src/app/__tests__/page.test.jsx
@@ -1,12 +1,12 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Page from "../src/app/page";
+import Page from "../page";
 
 describe("Page", () => {
   it("renders a heading", () => {
     render(<Page />);
 
-    const heading = screen.getByText("Save and see your changes instantly.");
+    const heading = screen.getByText('Save and see your changes instantly.');
 
     expect(heading).toBeInTheDocument();
   });
