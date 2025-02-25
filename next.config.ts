ee855beaa29655486794e421e96353b561dc2d90
Cozy-Habit <sophia.precker77@gmail.com>
2025-02-25 13:10:26 +0100

    add next.js app deploy to githup pages workflow

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  distDir: 'dist',
+  output: 'export', //the kind of build process next.js will perform
+  distDir: 'dist', //the name of the file in the baseDir where the build artifacts are going to be outputed
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
